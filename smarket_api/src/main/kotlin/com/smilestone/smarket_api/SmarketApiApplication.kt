d99c5725820d5689cd047693514215ec2b8e6ef8
jeongjin984 <philipwq3963@gmail.com>
2023-01-17 12:47:04 +0900

    feat : smarket_api initialized : back

---
+++ smarket_api/src/main/kotlin/com/smilestone/smarket_api/SmarketApiApplication.kt
@@ -0,0 +1,11 @@
+package com.smilestone.smarket_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmarketApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmarketApiApplication>(*args)
+}
