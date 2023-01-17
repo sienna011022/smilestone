79060b09631b0a81e42de5cf6bc8c06b5fb7b396
ubuntu <ubuntu@ubun2004.linuxvmimages.local>
2023-01-17 07:35:03 -0500

    feat : sendgin transport configuring : backEnd

---
+++ media_server/media_server/src/main/util/create_webRTC_transport.js
@@ -0,0 +1,47 @@
+export default async function createWebRtcTransport(router, callback) {
+    try {
+        const webRtcTransport_options = {
+            listenIps: [
+                {
+                    ip: '192.168.201.136', // replace with relevant IP address
+                }
+            ],
+            enableUdp: true,
+            enableTcp: true,
+            preferUdp: true,
+        }
+
+        // https://mediasoup.org/documentation/v3/mediasoup/api/#router-createWebRtcTransport
+        let transport = await router.createWebRtcTransport(webRtcTransport_options)
+
+        transport.on('dtlsstatechange', dtlsState => {
+            if (dtlsState === 'closed') {
+                transport.close()
+            }
+        })
+
+        transport.on('close', () => {
+            console.log('transport closed')
+        })
+
+        // send back to the client the following prameters
+        callback({
+            // https://mediasoup.org/documentation/v3/mediasoup-client/api/#TransportOptions
+            params: {
+                id: transport.id,
+                iceParameters: transport.iceParameters,
+                iceCandidates: transport.iceCandidates,
+                dtlsParameters: transport.dtlsParameters,
+            }
+        })
+
+        return transport
+    } catch (error) {
+        console.log(error)
+        callback({
+            params: {
+                error: error
+            }
+        })
+    }
+}
