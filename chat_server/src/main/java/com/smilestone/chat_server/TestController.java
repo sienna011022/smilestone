630d700ce54337bf5b74b2031efe8ec5e93b9c06
jeongjin984 <philipwq3963@gmail.com>
2023-02-17 11:41:14 +0900

    test

--- chat_server/src/main/java/com/smilestone/chat_server/TestController.java
+++ chat_server/src/main/java/com/smilestone/chat_server/TestController.java
@@ -1,34 +1,18 @@
 package com.smilestone.chat_server;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
-    private final RabbitTemplate rabbitTemplate;
+    private final SimpMessagingTemplate template;
 
-    private final static String CHAT_EXCHANGE_NAME = "chat.exchange";
-    private final static String CHAT_QUEUE_NAME = "chat.queue";
-
-    @MessageMapping("chat.enter.{chatRoomId}")
+    @MessageMapping("chat.{chatRoomId}")
     public void sendEnterMessage(ChatMessage message, @DestinationVariable String chatRoomId) {
-        rabbitTemplate.convertAndSend(CHAT_EXCHANGE_NAME, "chatroom." + chatRoomId, message);
-    }
-
-    @MessageMapping("chat.message.{chatRoomId}")
-    public void sendMessage(ChatMessage message, @DestinationVariable String chatRoomId) {
-        rabbitTemplate.convertAndSend(CHAT_EXCHANGE_NAME, "chatroom." + chatRoomId, message);
-    }
-
-    @RabbitListener(queues = CHAT_QUEUE_NAME)
-    public void receive(ChatMessage message){
-        System.out.println("received : " + message.getMessage());
+        template.convertAndSend("/chat/" + chatRoomId, message);
     }
 }
--- chat_server/src/main/java/com/smilestone/chat_server/WebSocketConfig.java
+++ chat_server/src/main/java/com/smilestone/chat_server/WebSocketConfig.java
@@ -6,19 +6,20 @@ import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setSendTimeLimit(15 * 1000).setSendBufferSizeLimit(512 * 1024);
+        registration.setMessageSizeLimit(128 * 1024);
+    }
+
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
-                .setRelayHost("192.168.201.128")
-                .setClientLogin("test")
-                .setClientPasscode("test")
-                .setSystemLogin("test")
-                .setSystemPasscode("test")
-                .setVirtualHost("/");
+        registry.enableSimpleBroker("/chat");
         registry.setApplicationDestinationPrefixes("/pub");
         registry.setPathMatcher(new AntPathMatcher("."));
     }
